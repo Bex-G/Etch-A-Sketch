0f204c371f81288c767df61c11371d252b8445f7
Bex G <rkmoody1@gmail.com>
2023-03-27 10:27:08 -0600

    Add event listener that colors box divs with mouseover.

--- etchASketch.js
+++ etchASketch.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('#container');
 
-const box = document.createElement('box')
-
 for (i = 0; i < 256; i++) {
-    const boxClone = box.cloneNode(true);
-    const boxContent = document.createTextNode('\u00A0');
-    boxClone.appendChild(boxContent);
-    container.appendChild(boxClone);
+    const box = document.createElement('box')
+    const boxContent = document.createTextNode('\u00A0')
+
+    box.appendChild(boxContent);
+    container.appendChild(box);
+
+    box.addEventListener('mouseover', (e) => {
+        e.target.style.background = 'black';
+    });
 }
 

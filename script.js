cc14124dc6080d78e8dff6a609a8808789f80e8a
Bex G <rkmoody1@gmail.com>
2023-03-28 09:38:45 -0600

    Create function that resets sketchpad when button is clicked.

--- script.js
+++ script.js
@@ -3,40 +3,47 @@ const container = document.querySelector('#container');
 for (i = 0; i < 256; i++) {
     const box = document.createElement('box')
     const boxContent = document.createTextNode('\u00A0')
-
-    box.classList.add('boxes');
-
+    
     box.appendChild(boxContent);
     container.appendChild(box);
 
+
     box.addEventListener('mouseover', (e) => {
         e.target.style.background = 'black';
-    });
+    })
+};
+
+function clearSketchpad() {
+    container.innerHTML = '';
+};
+
+function newSketchpad(n) {   
+
+    for (i = 0; i < n*n; i++) {
+        const box = document.createElement('box')
+        const boxContent = document.createTextNode('\u00A0')
+
+        box.appendChild(boxContent);
+        container.appendChild(box);
+
+        box.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'black';
+        })
+    }
 };
 
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    
-    let n = parseInt(prompt("How many squares would you like on either side?"))
+
+    clearSketchpad();
+
+    let n = parseInt(prompt("How many pixels would you like on either side?"));
     
     if (n > 100) {
-        let n = parseInt(prompt("Sorry, please choose a number that is less than 100."))
+        let n = parseInt(prompt("Sorry, please choose a number that is less than 100."));
         newSketchpad(n);
     }
-    newSketchpad()
+    
+    newSketchpad(n);
 });
-
-function newSketchpad(n) {    
-
-for (i = 0; i < n*n; i++) {
-    const box = document.createElement('box')
-    const boxContent = document.createTextNode('\u00A0')
-
-    box.appendChild(boxContent);
-    container.appendChild(box);
-
-    box.addEventListener('mouseover', (e) => {
-        e.target.style.background = 'black';
-    });
-}};

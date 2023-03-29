502f83ccfd1b0e585b948efdcfea026314c01f7c
Bex G <rkmoody1@gmail.com>
2023-03-29 16:33:57 -0600

    Remove container click event listener and add functions to determine flex-basis to style boxes as prompted by button.

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const container = document.querySelector('#container');
 
-container.addEventListener('click', () => {
-    startSketchPad();
-});
-
-function startSketchPad() {
-
-for (i = 0; i < 256; i++) {
+for (i = 0; i < 16 * 16; i++) {
     const box = document.createElement('box')
     const boxContent = document.createTextNode('\u00A0')
+    
+    box.style.height = '6.25%';
+    box.style.width = '6.25%';
     
     box.appendChild(boxContent);
     container.appendChild(box);
@@ -17,28 +14,40 @@ for (i = 0; i < 256; i++) {
     box.addEventListener('mouseover', (e) => {
         e.target.style.background = 'black';
     })
-}};
+};
 
 function clearSketchpad() {
     container.innerHTML = '';
 };
 
+function formatAsPercentage(n) {
+    return new Intl.NumberFormat('defult', {
+        style: 'percent',
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 2
+    }).format(n / 100);
+}
+
+function findFlexBasis(n) {
+    let pixelsPerRow = 100 / n;
+    return formatAsPercentage(pixelsPerRow);
+}
+
 function newSketchpad(n) {   
 
-    container.addEventListener('click', () => {
+    for (i = 0; i < n*n; i++) {
+        const box = document.createElement('box')
+        const boxContent = document.createTextNode('\u00A0')
+        
+        box.style.flexBasis = findFlexBasis(n);
 
-        for (i = 0; i < n*n; i++) {
-            const box = document.createElement('box')
-            const boxContent = document.createTextNode('\u00A0')
+        box.appendChild(boxContent);
+        container.appendChild(box);
 
-            box.appendChild(boxContent);
-            container.appendChild(box);
-
-            box.addEventListener('mouseover', (e) => {
-                e.target.style.background = 'black';
-            })
-        }
-    })
+        box.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'black';
+        })
+    }
 };
 
 const button = document.querySelector('button');
@@ -53,6 +62,5 @@ button.addEventListener('click', () => {
         let n = parseInt(prompt("Sorry, please choose a number that is less than 100."));
         newSketchpad(n);
     }
-    
     newSketchpad(n);
 });
